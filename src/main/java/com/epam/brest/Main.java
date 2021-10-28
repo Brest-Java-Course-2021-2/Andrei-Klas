2d716ffd272ecb1fc15afd4a037d4c917d4119dc
andreyklass94 <andreyklass94@gmail.com>
2021-10-28 15:12:46 +0300

    Calc 2.0 - added Alexander logic, using JSON files; added Spring

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -1,32 +1,36 @@
 package com.epam.brest;
 
-import com.epam.brest.calc.Calc;
+import com.epam.brest.files.JSONFileReader;
 import com.epam.brest.calc.CalcImpl;
+import com.epam.brest.model.ReadDataState;
+import com.epam.brest.model.Status;
+import com.epam.brest.model.StatusType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        BigDecimal weight, pricePerKG, length, pricePerKm;
+        BigDecimal weight, length;
 
-        try (Scanner scanner = new Scanner(System.in)){
-            do {
-                weight = getValueFromCon(scanner, "Enter m:");
-                length = getValueFromCon(scanner, "Enter length:");
-                System.out.println("Result = " + CalcImpl.handleFromFile(weight, length));
-                System.out.println("Enter 'q' to exit or 'c' for continue");
-            }while(!scanner.hasNext("q"));
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        JSONFileReader JSONfReader = (JSONFileReader) applicationContext.getBean("fileReader");
+
+        Map<Integer, BigDecimal> pricePerKgMap = JSONfReader.readData("/PriceForKg.json");
+        Map<Integer, BigDecimal> pricePerKmMap = JSONfReader.readData("/PriceForKm.json");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            Status currentStatus = new ReadDataState(scanner, pricePerKgMap, pricePerKmMap);
+            while (currentStatus.getType() != StatusType.EXIT) {
+                System.out.println("Current system status: " + currentStatus.getType());
+                currentStatus = currentStatus.handle();
+            }
         }
     }
-
-    private static BigDecimal getValueFromCon(Scanner scanner, String outputMessage) {
-        BigDecimal enteredValue;
-        System.out.print(outputMessage);
-        enteredValue = scanner.nextBigDecimal();
-        return enteredValue;
-    }
-
 
 }
--- src/main/java/com/epam/brest/calc/Calc.java
+++ src/main/java/com/epam/brest/calc/Calc.java
@@ -3,9 +3,7 @@ package com.epam.brest.calc;
 import java.math.BigDecimal;
 
 public interface Calc {
-    BigDecimal handle(BigDecimal weight, BigDecimal pricePerKG, BigDecimal length, BigDecimal pricePerKm);
 
-    static BigDecimal handleFromFile(BigDecimal weight, BigDecimal length) {
-        return null;
-    }
+    BigDecimal handle(BigDecimal weight, BigDecimal pricePerKg, BigDecimal length, BigDecimal pricePerKm);
+
 }
